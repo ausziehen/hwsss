bfe2cf6cbb98ca19d1618e4dee75ad6274bd6aa0
ausziehen <marimaki@bk.ru>
2023-07-24 20:12:54 +0300

    1

---
+++ ex0001/Program.cs
@@ -0,0 +1,40 @@
+﻿// Console.Write("Input array length: ");
+// int arr = Convert.ToInt32(Console.ReadLine());
+// int[] Array = new int[arr];
+
+// void Massive(int arr)
+// {   for (int i = 0; i < arr; i++)
+//     {
+//         Console.WriteLine($"Input {i + 1} element of massive ");
+//         Array[i] = Convert.ToInt32(Console.ReadLine());
+//     }
+// }
+// int Amount(int[] Array)
+// {   int i = 0;
+//     int sum = 0;
+//     while (i < Array.Length)
+//     {   if (Array[i] > 0)
+//             sum = sum + 1;
+//         i = i + 1;
+//     }
+//     return sum;
+// }
+// Massive(arr);
+// Console.WriteLine("");
+// Console.Write($"Numbers greater than zero: {Amount(Array)}");
+
+
+
+// Console.WriteLine("введите значение b1");
+// double b1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите число k1");
+// double k1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите значение b2");
+// double b2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите число k2");
+// double k2 = Convert.ToInt32(Console.ReadLine());
+
+// double x = (-b2 + b1)/(-k1 + k2);
+// double y = k2 * x + b2;
+
+// Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
